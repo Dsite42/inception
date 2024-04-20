e68e74bda045ee5ded3c523152c1f7190dd23cb5
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2024-04-20 13:18:24 +0200

    added more comments for mariaDB

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,15 +1,21 @@
-#!bin/sh
+#!/bin/sh
 
+# Checks if the database already exists 
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
-	# Initialize the database
+	# Initialize the database, specifing the user and the data directory
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
 	# replace variables
+	# search for the placeholder DB_NAME in the db_create.sql script and replaces
+	# it with the actual database name stored in the $DB_NAME environment variable. 
 	sed -i "s/DB_NAME/$DB_NAME/g" /var/mariadb/db_create.sql
 	sed -i "s/DB_USER_PW/$DB_USER_PW/g" /var/mariadb/db_create.sql
 	sed -i "s/DB_USER_NAME/$DB_USER_NAME/g" /var/mariadb/db_create.sql
 	sed -i "s/DB_ROOT_PW/$DB_ROOT_PW/g" /var/mariadb/db_create.sql
 fi
 
+# It replaces itself with the command specified in CMD (see Dockerfile) —in this case, starting the MariaDB
+# server with the given options. This ensures that the MariaDB server runs as the main process in the
+# container (PID 1) and can directly handle signals sent to the container, like stop requests.
 exec "$@"
